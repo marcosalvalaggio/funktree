cdda380366a26938ee1f8c10d9b6f8613d9b0e15
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-08-26 12:03:09 +0200

    wip

---
+++ lua/clang.lua
@@ -0,0 +1,60 @@
+
+local function clang(root_lines, buf)
+    local func_pattern = "^[A-Za-z_][A-Za-z0-9_]*%s*[*]*%s+([A-Za-z_][A-Za-z0-9_]*)"
+    local struct_pattern = "struct%s+([A-Za-z_][A-Za-z0-9_]*)"
+    local enum_pattern = "enum%s+([A-Za-z_][A-Za-z0-9_]*)"
+    local typedef_name_pattern = "}%s*([A-Z_][a-z0-9_]*)%s*;"
+    local typedef_struct_position_pattern = "typedef struct"
+    local typedef_enum_position_pattern = "typedef enum"
+    local typedef_struct_position = nil
+    local typedef_enum_position = nil
+    local typedef_ongoing = false
+    local reduced_lines = {}
+    local status = false
+    for i, line in ipairs(root_lines) do
+        local struct_name = line:match(struct_pattern)
+        local enum_name = line:match(enum_pattern)
+        local typedef_name = line:match(typedef_name_pattern)
+        local typedef_struct_position_match = line:match(typedef_struct_position_pattern)
+        local typedef_enum_position_match = line:match(typedef_enum_position_pattern)
+
+        if typedef_ongoing then
+            if typedef_name then
+                typedef_ongoing = false
+                if typedef_struct_position then
+                    table.insert(reduced_lines, string.format("struct: %s, line: %d", typedef_name, typedef_struct_position))
+                    status = true
+                elseif typedef_enum_position then
+                    table.insert(reduced_lines, string.format("enum: %s, line: %d", typedef_name, typedef_enum_position))
+                    status = true
+                end
+            end
+        elseif typedef_struct_position_match then
+           typedef_struct_position = i
+           typedef_enum_position = nil
+           typedef_ongoing = true
+        elseif typedef_enum_position_match then
+            typedef_enum_position = i
+            typedef_struct_position = nil
+            typedef_ongoing = true
+        elseif struct_name then
+            table.insert(reduced_lines, string.format("struct: %s, line: %d", struct_name, i))
+            status = true
+        elseif enum_name then
+            table.insert(reduced_lines, string.format("enum: %s, line: %d", enum_name, i))
+            status = true
+        else
+            local function_name = line:match(func_pattern)
+            if function_name and function_name ~= "struct" and function_name ~= "enum" then
+                table.insert(reduced_lines, string.format("ƒ: %s, line: %d", function_name, i))
+                status = true
+            end
+        end
+    end
+    if not status then
+        table.insert(reduced_lines, "No functions or structs in this file")
+    end
+    vim.api.nvim_buf_set_lines(buf, 1, -1, false, reduced_lines)
+end
+
+return clang
--- lua/funktree.lua
+++ lua/funktree.lua
@@ -1,12 +1,12 @@
 local api = vim.api
 local buf, win
-local position = 0
 local root_lines = {}
 local root_win
 local file_extension
 local golang = require("golang")
 local pylang = require("pylang")
 local lualang = require("lualang")
+local clang = require("clang")
 
 local function center(str)
     local width = api.nvim_win_get_width(0)
@@ -72,65 +72,6 @@ local function close_window()
 end
 
 
-local function clang(root_lines)
-    local func_pattern = "^[A-Za-z_][A-Za-z0-9_]*%s*[*]*%s+([A-Za-z_][A-Za-z0-9_]*)"
-    local struct_pattern = "struct%s+([A-Za-z_][A-Za-z0-9_]*)"
-    local enum_pattern = "enum%s+([A-Za-z_][A-Za-z0-9_]*)"
-    local typedef_name_pattern = "}%s*([A-Z_][a-z0-9_]*)%s*;"
-    local typedef_struct_position_pattern = "typedef struct"
-    local typedef_enum_position_pattern = "typedef enum"
-    local typedef_struct_position = nil
-    local typedef_enum_position = nil
-    local typedef_ongoing = false
-    local reduced_lines = {}
-    local status = false
-    for i, line in ipairs(root_lines) do
-        local struct_name = line:match(struct_pattern)
-        local enum_name = line:match(enum_pattern)
-        local typedef_name = line:match(typedef_name_pattern)
-        local typedef_struct_position_match = line:match(typedef_struct_position_pattern)
-        local typedef_enum_position_match = line:match(typedef_enum_position_pattern)
-
-        if typedef_ongoing then
-            if typedef_name then
-                typedef_ongoing = false
-                if typedef_struct_position then
-                    table.insert(reduced_lines, string.format("struct: %s, line: %d", typedef_name, typedef_struct_position))
-                    status = true
-                elseif typedef_enum_position then
-                    table.insert(reduced_lines, string.format("enum: %s, line: %d", typedef_name, typedef_enum_position))
-                    status = true
-                end
-            end
-        elseif typedef_struct_position_match then
-           typedef_struct_position = i
-           typedef_enum_position = nil
-           typedef_ongoing = true
-        elseif typedef_enum_position_match then
-            typedef_enum_position = i
-            typedef_struct_position = nil
-            typedef_ongoing = true
-        elseif struct_name then
-            table.insert(reduced_lines, string.format("struct: %s, line: %d", struct_name, i))
-            status = true
-        elseif enum_name then
-            table.insert(reduced_lines, string.format("enum: %s, line: %d", enum_name, i))
-            status = true
-        else
-            local function_name = line:match(func_pattern)
-            if function_name and function_name ~= "struct" and function_name ~= "enum" then
-                table.insert(reduced_lines, string.format("ƒ: %s, line: %d", function_name, i))
-                status = true
-            end
-        end
-    end
-    if not status then
-        table.insert(reduced_lines, "No functions or structs in this file")
-    end
-    vim.api.nvim_buf_set_lines(buf, 1, -1, false, reduced_lines)
-end
-
-
 local function update_view(root_lines)
     print(file_extension)
     if file_extension == "py" then
@@ -141,7 +82,7 @@ local function update_view(root_lines)
         -- golang(root_lines)
         golang(root_lines, buf)
     elseif file_extension == "c" or file_extension == "h" then
-        clang(root_lines)
+        clang(root_lines, buf)
     end
 end
 
@@ -174,7 +115,6 @@ end
 
 
 local function funktree()
-    position = 0
     open_window()
     update_view(root_lines)
     set_mappings()
