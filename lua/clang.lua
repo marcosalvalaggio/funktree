667bb67e70f4b70649bd5f5a261ed23a567780aa
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-08-30 13:44:20 +0200

    wip

--- lua/clang.lua
+++ lua/clang.lua
@@ -1,7 +1,7 @@
 
 local function clang(root_lines, buf)
-    local func_pattern = "(void|int|float|double|char|bool|long|short)[ \t]*\\*?[ \t]*(\\*\\*[ \t]*)?[ \t]*([A-Za-z_][A-Za-z0-9_]*)%s*%("
-    local method_pattern = "   (void|int|float|double|char|bool|long|short)[ \t]*\\*?[ \t]*(\\*\\*[ \t]*)?[ \t]*([A-Za-z_][A-Za-z0-9_]*)%s*%("
+    local func_pattern = "(%w+)[ \t]*\\*?[ \t]*(\\*\\*[ \t]*)?[ \t]*([A-Za-z_][A-Za-z0-9_]*)%s*%("
+    local method_pattern = "    (%w+)[ \t]*\\*?[ \t]*(\\*\\*[ \t]*)?[ \t]*([A-Za-z_][A-Za-z0-9_]*)%s*%("
     local struct_pattern = "struct%s+([A-Za-z_][A-Za-z0-9_]*)"
     local enum_pattern = "enum%s+([A-Za-z_][A-Za-z0-9_]*)"
     local class_pattern = "class%s+([A-Za-z_][A-Za-z0-9_]*)"
