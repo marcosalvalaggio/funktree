3014e38862025793d2b2b194ef57311aedcccc5e
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-09-03 10:51:37 +0200

    wip

--- lua/golang.lua
+++ lua/golang.lua
@@ -4,14 +4,23 @@ local function golang(root_lines, buf)
     local method_pattern = "func%s*%([^)]*%)%s+([A-Za-z][A-Za-z0-9]*)%s*%(%s*%)"
     local struct_pattern = "type%s+([A-Za-z][A-Za-z0-9]*)%s+struct"
     local interface_pattern = "type%s+([A-Za-z][A-Za-z0-9]*)%s+interface"
+    local comment_pattern = "//"
     local reduced_lines = {}
     local status = false
     for i, line in ipairs(root_lines) do
         local struct_name = line:match(struct_pattern)
         local interface_name = line:match(interface_pattern)
         if struct_name then
-            table.insert(reduced_lines, string.format("struct: %s, line: %d", struct_name, i))
-            status = true
+            local comment_start, struct_start = line:find(comment_pattern), line:find(struct_pattern)
+            if comment_start then
+                if comment_start and struct_start and comment_start > struct_start then
+                    table.insert(reduced_lines, string.format("struct: %s, line: %d", struct_name, i))
+                    status = true
+                end
+            else
+                table.insert(reduced_lines, string.format("struct: %s, line: %d", struct_name, i))
+                status = true
+            end
         elseif interface_name then
             table.insert(reduced_lines, string.format("interface: %s, line: %d", interface_name, i))
             status = true
