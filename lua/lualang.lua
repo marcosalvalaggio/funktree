fa46f0fe766f06e6f0040121cd25f498f4c97e59
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-09-03 10:35:21 +0200

    wip

--- lua/lualang.lua
+++ lua/lualang.lua
@@ -1,22 +1,13 @@
 
 local function lualang(root_lines, buf)
     local func_pattern = "function%s+([%w_]+)%s*%([^)]*%)"
-    local comment_pattern = "--"
     local reduced_lines = {}
     local status = false
     for i, line in ipairs(root_lines) do
         local func_name = line:match(func_pattern)
         if func_name then
-            local comment_start, func_start = line:find(comment_pattern), line:find(func_pattern)
-            if comment_start then
-                if comment_start and func_start and comment_start > func_start then
-                    table.insert(reduced_lines, string.format("ƒ: %s, line: %d", func_name, i))
-                    status = true
-                end
-            else
-                table.insert(reduced_lines, string.format("ƒ: %s, line: %d", func_name, i))
-                status = true
-            end
+            table.insert(reduced_lines, string.format("ƒ: %s, line: %d", func_name, i))
+            status = true
         end
     end
     if not status then
